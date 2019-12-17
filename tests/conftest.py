647e5d0f44f6fff8362a39b70519441c03cbaf48
Adrian <a.vandier@softlaw.digital>
2019-12-17 14:05:57 +0100

    extract fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
-from flask import Flask
-
-
-@pytest.fixture(scope="session")
-def app():
-    app = Flask(__name__)
-    return app
-
-
-@pytest.yield_fixture(scope="session")
-def client(app):
-    return app.test_client()
+from .fixtures import app
+from .fixtures import client
+from .fixtures import base
+from .fixtures import person_tag_model
+from .fixtures import person_single_tag_model
+from .fixtures import string_json_attribute_person_model
+from .fixtures import person_model
+from .fixtures import computer_model
+from .fixtures import engine
+from .fixtures import session
+from .fixtures import person
+from .fixtures import person_2
+from .fixtures import computer
+from .fixtures import dummy_decorator
+from .fixtures import person_tag_schema
+from .fixtures import person_single_tag_schema
+from .fixtures import address_schema
+from .fixtures import string_json_attribute_person_schema
+from .fixtures import person_schema
+from .fixtures import computer_schema
+from .fixtures import before_create_object
+from .fixtures import before_update_object
+from .fixtures import before_delete_object
+from .fixtures import person_list
+from .fixtures import person_detail
+from .fixtures import person_computers
+from .fixtures import person_list_raise_jsonapiexception
+from .fixtures import person_list_raise_exception
+from .fixtures import person_list_response
+from .fixtures import person_list_without_schema
+from .fixtures import query
+from .fixtures import computer_list
+from .fixtures import computer_detail
+from .fixtures import computer_owner
+from .fixtures import string_json_attribute_person_detail
+from .fixtures import string_json_attribute_person_list
+from .fixtures import api_blueprint
+from .fixtures import register_routes
+from .fixtures import get_object_mock
